a745e8a9330ee0f218268911d2df4957071b3381
baz241 <manulmanulov425@gmail.com>
2024-07-18 11:27:25 +0300

    feat: final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = 0;
+    n = (n < 0) ? -n : n;
+    while (n > 0) {
+        result = (result * 10) + (n % 10);
+        n = Math.trunc(n / 10);
+    }
+    return result;
+};
